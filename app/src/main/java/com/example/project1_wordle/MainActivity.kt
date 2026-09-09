5d68cd0c61993affa803439fb98d28fe4c14cc20
Marquise <marzphillip@gmail.com>
2026-09-09 15:20:16 -0400

    finished bare features

--- app/src/main/java/com/example/project1_wordle/MainActivity.kt
+++ app/src/main/java/com/example/project1_wordle/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.project1_wordle
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,69 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find UI elements by their ID
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val sendButton = findViewById<Button>(R.id.guessButton)
+        val displayText = findViewById<TextView>(R.id.displayText)
+        val answerText = findViewById<TextView>(R.id.answerView)
+        answerText.isVisible = false
+
+        var guessCount = 0
+        val history = StringBuilder()
+
+        sendButton.setOnClickListener {
+            guessCount++
+            val userInput = inputField.text.toString().uppercase()
+            val guessResult = checkGuess(userInput)
+
+            if (guessCount < 7) {            // Build the new lines for this guess
+                val newEntry =
+                    "Guess #$guessCount \t\t\t\t\t\t\t\t\t\t\t\t\t\t $userInput\nGuess #$guessCount  Check \t\t\t $guessResult\n"
+
+                // Append to history
+                history.append(newEntry)
+
+                // Update the TextView
+                displayText.text = history.toString()
+            } else {
+                answerText.text = "$wordToGuess"
+                answerText.isVisible = true
+            }
+        }
+
     }
+}
+
+
+
+/**
+ * Parameters / Fields:
+ *   wordToGuess : String - the target word the user is trying to guess
+ *   guess : String - what the user entered as their guess
+ *
+ * Returns a String of 'O', '+', and 'X', where:
+ *   'O' represents the right letter in the right place
+ *   '+' represents the right letter in the wrong place
+ *   'X' represents a letter not in the target word
+ */
+
+val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+
+
+
+private fun checkGuess(guess: String) : String {
+    var result = ""
+    for (i in 0..3) {
+        if (guess[i] == wordToGuess[i]) {
+            result += "O"
+        }
+        else if (guess[i] in wordToGuess) {
+            result += "+"
+        }
+        else {
+            result += "X"
+        }
+    }
+    return result
 }
